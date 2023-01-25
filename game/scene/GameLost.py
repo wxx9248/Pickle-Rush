77778f62a16660fe0a776b265c2ea0962e061022
Jake Wang <wxx9248@qq.com>
2023-01-25 02:17:39 -0500

    Improve Graphics for Menu/GameWin/GameLost Scene (#28)

--- game/scene/GameLost.py
+++ game/scene/GameLost.py
@@ -7,6 +7,7 @@ from asset.AssetObjectFactory import AssetObjectFactory
 from core.object_model.Atlas import Atlas
 from core.object_model.Layer import Layer
 from core.object_model.Scene import Scene
+from core.object_model.Sprite import Sprite
 from core.object_model.Text import Text
 from event.CustomEventTypes import CustomEventTypes
 from game.atlas.MenuSelectorAtlas import MenuSelectorAtlas
@@ -19,26 +20,32 @@ class GameLost(Scene):
         super().__init__(size)
         self.__retry_scene = retry_scene
 
-        game_lost_text_sprite: Text = AssetObjectFactory().new_asset_object("asset.text.game-lost")
+        self.background = Atlas(AssetObjectFactory().new_asset_object("asset.sprite.menu.background"))
+        self.background.scale_to(size)
+
+        game_lost_text_sprite: Sprite = AssetObjectFactory().new_asset_object("asset.sprite.game-lost")
         game_lost_text_atlas = Atlas(game_lost_text_sprite)
         game_lost_text_atlas.position_x = util.center(self.size, game_lost_text_sprite.surface.get_size())[0]
-        game_lost_text_atlas.position_y = 150
+        game_lost_text_atlas.position_y = 100
 
-        retry_text_sprite: Text = AssetObjectFactory().new_asset_object("asset.text.retry")
+        retry_text_sprite: Sprite = AssetObjectFactory().new_asset_object("asset.sprite.retry")
         retry_text_atlas = Atlas(retry_text_sprite)
-        retry_text_atlas.position_x = util.center(self.size, retry_text_sprite.surface.get_size())[0]
+        retry_text_atlas.scale = (0.6, 0.6)
+        retry_text_atlas.position_x = util.center(self.size, retry_text_sprite.surface.get_size())[0] + 40
         retry_text_atlas.position_y = 450
 
-        back_to_menu_text_sprite: Text = AssetObjectFactory().new_asset_object("asset.text.back-to-menu")
+        back_to_menu_text_sprite: Sprite = AssetObjectFactory().new_asset_object("asset.sprite.back-to-menu")
         back_to_menu_text_atlas = Atlas(back_to_menu_text_sprite)
-        back_to_menu_text_atlas.position_x = util.center(self.size, back_to_menu_text_sprite.surface.get_size())[0]
+        back_to_menu_text_atlas.scale = (0.6, 0.6)
+        back_to_menu_text_atlas.position_x = util.center(self.size, back_to_menu_text_sprite.surface.get_size())[0] + 115
         back_to_menu_text_atlas.position_y = 550
 
         cursor_sprite_atlas = MenuSelectorAtlas()
+        cursor_sprite_atlas.scale = (0.04, 0.04)
         cursor_sprite_atlas.position_x = 400
         cursor_sprite_atlas.add_anchor(
             (retry_text_atlas.position_y - 6, lambda: self.retry()),
-            (back_to_menu_text_atlas.position_y - 6, lambda: self.back_to_menu())
+            (back_to_menu_text_atlas.position_y + 4, lambda: self.back_to_menu())
         )
 
         layer = Layer(game_lost_text_atlas, retry_text_atlas, back_to_menu_text_atlas, cursor_sprite_atlas)
--- game/scene/GameWin.py
+++ game/scene/GameWin.py
@@ -7,6 +7,7 @@ from asset.AssetObjectFactory import AssetObjectFactory
 from core.object_model.Atlas import Atlas
 from core.object_model.Layer import Layer
 from core.object_model.Scene import Scene
+from core.object_model.Sprite import Sprite
 from core.object_model.Text import Text
 from event.CustomEventTypes import CustomEventTypes
 from game.atlas.MenuSelectorAtlas import MenuSelectorAtlas
@@ -18,20 +19,25 @@ class GameWin(Scene):
         super().__init__(size)
         self.__next_level_scene = next_level_scene
 
-        game_win_text_sprite: Text = AssetObjectFactory().new_asset_object("asset.text.game-win")
-        game_win_text_atlas = Atlas(game_win_text_sprite)
-        game_win_text_atlas.position_x = util.center(self.size, game_win_text_sprite.surface.get_size())[0]
-        game_win_text_atlas.position_y = 150
+        self.background = Atlas(AssetObjectFactory().new_asset_object("asset.sprite.menu.background"))
+        self.background.scale_to(size)
 
-        next_level_text_sprite: Text = AssetObjectFactory().new_asset_object("asset.text.next-level")
+        game_win_text_sprite: Sprite = AssetObjectFactory().new_asset_object("asset.sprite.game-win")
+        game_win_text_atlas = Atlas(game_win_text_sprite)
+        game_win_text_atlas.position_x = util.center(self.size, game_win_text_sprite.surface.get_size())[0] - 20
+        game_win_text_atlas.position_y = 100
+
+        next_level_text_sprite: Sprite = AssetObjectFactory().new_asset_object("asset.sprite.next-level")
         next_level_text_atlas = Atlas(next_level_text_sprite)
-        next_level_text_atlas.position = util.center(self.size, next_level_text_sprite.surface.get_size())[0]
+        next_level_text_atlas.scale = (0.6, 0.6)
+        next_level_text_atlas.position = util.center(self.size, next_level_text_sprite.surface.get_size())[0] + 90
         next_level_text_atlas.position_y = 500
 
         cursor_sprite_atlas = MenuSelectorAtlas()
+        cursor_sprite_atlas.scale = (0.04, 0.04)
         cursor_sprite_atlas.position_x = 450
         cursor_sprite_atlas.add_anchor(
-            (next_level_text_atlas.position_y - 6, lambda: self.to_next_level()),
+            (next_level_text_atlas.position_y + 4, lambda: self.to_next_level()),
         )
 
         layer = Layer(game_win_text_atlas)
